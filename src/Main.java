cccae05ea9c9ccc65e0e9789764ba8213ae4859c
SiNii <devenn007@gmail.com>
2021-07-06 18:06:09 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = ("1- укдяр2оп оп 3алсич4 ына5сип6ан7 ьсе8дз9");
+        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern1 = Pattern.compile("\\D+");
+        Matcher matcher = pattern.matcher(text);
+        Matcher matcher1 = pattern1.matcher(text);
+
+        String result = (matcher.replaceAll(""));
+        StringBuilder stringBuilder = new StringBuilder(result);
+
+        System.out.println(stringBuilder.reverse() + " " + matcher1.replaceAll("") );
+
+    }
+}
